122b255b9b001e6aaae090734a13b4aca9e18aeb
Ivan-rom <rom_1van@icloud.com>
2024-10-21 17:22:52 +0600

    feat: implement building via webpack and running in production mode

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/app.ts",
+  target: "node",
+  mode: "production",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+  },
+};
